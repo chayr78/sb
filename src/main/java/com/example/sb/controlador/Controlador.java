a176726d5d5443b9328da699d54da8422e356b78
chayr78 <kchayrez123@gmail.com>
2026-07-26 14:39:51 -0600

    Controlador subido por chay

--- src/main/java/com/example/sb/controlador/Controlador.java
+++ src/main/java/com/example/sb/controlador/Controlador.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+// Controlador subido por chay
 @RestController
 public class Controlador {
     @GetMapping("/infoo/{nombre}/{edad}/{estatura}/{peso}")
